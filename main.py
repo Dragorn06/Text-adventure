b96a05fcfcce3fe0cbe77c5fac555e273531258c
1t2e3d <8wilsoe@royalhospitalschool.org>
2020-09-17 09:14:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from random import randint
+
+import Levels
+
+print("you wake up in a room")
+print("There is a table with a box on it to the left of you. There is also a door to the right of you.")
+print("write commands in normal English")
+
+def level0(): 
+  health = 10
+  
+  openbox = ("open the box ")
+  inopenbox = input("")
+  
+  opendoor = ("open the door")
+  inopendoor = input("")
+
+while Level_0:
+  level0()
+
+
+
+
+
+  
+  
+  
